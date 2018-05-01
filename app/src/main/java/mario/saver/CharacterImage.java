02a9a2e0b407faf4d28c181387ded391e7dfcf4b
lukakralj <luka.kralj2@gmail.com>
2018-05-01 22:00:16 +0100

    Added comments.

--- app/src/main/java/mario/saver/CharacterImage.java
+++ app/src/main/java/mario/saver/CharacterImage.java
@@ -3,9 +3,11 @@ package mario.saver;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-
 import java.util.Random;
 
+/**
+ * Represents the character that is moving on the screen.
+ */
 public class CharacterImage {
     private Bitmap image;
     private int x, y;
@@ -14,6 +16,11 @@ public class CharacterImage {
     private int screenWidth;
     private int screenHeight;
 
+    /**
+     * Create new character.
+     *
+     * @param bmp This image appears on the screen and represents the character.
+     */
     public CharacterImage(Bitmap bmp) {
         image = bmp;
 
@@ -27,14 +34,23 @@ public class CharacterImage {
         y = rand.nextInt(screenHeight - image.getHeight());
     }
 
+    /**
+     * Draw the character on the canvas.
+     *
+     * @param canvas Canvas to draw the character on.
+     */
     public void draw(Canvas canvas) {
         canvas.drawBitmap(image, x, y, null);
     }
 
+    /**
+     * Update the character's properties.
+     */
     public void update() {
         x += xVelocity;
         y += yVelocity;
 
+        // If we reached the end of the screen the character needs to start moving to the other direction.
         if ((x > screenWidth - image.getWidth()) || (x < 0)) {
             xVelocity *= -1;
         }
